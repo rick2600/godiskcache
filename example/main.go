6e75b6f21ed6ff49e1f055021ba1ec145677689c
rick2600 <ricks2600@gmail.com>
2023-06-08 02:40:38 -0300

    fix ident

--- example/main.go
+++ example/main.go
@@ -2,16 +2,15 @@ package main
 
 
 import (
-	"fmt"
-	"time"
-	"os"
-	"path/filepath"
+    "fmt"
+    "time"
+    "os"
+    "path/filepath"
     "github.com/rick2600/godiskcache/pkg"
 )
 
 
 func main() {
-
     path := filepath.Join(os.TempDir(), "diskcache")
     diskcache, _ := diskcache.NewDiskcache(path, 2*time.Minute)
     fmt.Println(diskcache.Directory)
@@ -19,10 +18,9 @@ func main() {
     key := "test"
     data, found := diskcache.Get(key)
     if found {
-    	fmt.Println("Found", string(data))
+        fmt.Println("Found", string(data))
     } else {
-    	fmt.Println("Not found")
-    	diskcache.Set(key, []byte("Hello from cache\n"))
+        fmt.Println("Not found")
+        diskcache.Set(key, []byte("Hello from cache\n"))
     }
-
 }
